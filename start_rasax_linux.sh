ec5701ffb7f00b490359a62927010bef0900298e
Damien Dallon <damien.dallon@alumni.univ-avignon.fr>
2022-12-05 17:16:06 +0100

    add scripts to start application

---
+++ start_rasax_linux.sh
@@ -0,0 +1,4 @@
+conda activate rasax
+cd rasaxProject
+rasa run actions &
+rasa x
---
+++ start_rasax_windows.bat
@@ -0,0 +1,4 @@
+cd rasaxProject
+call conda activate rasax
+start "Action Server" call rasa run actions
+start "RasaX" call rasa x
