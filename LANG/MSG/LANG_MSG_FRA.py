deb78d6fa1dc5f7b014cc644ea17e31cfd1e4bc9
Damien Dallon <damien.dallon@alumni.univ-avignon.fr>
2022-12-07 10:50:16 +0100

    NLU : modify sentences for time

--- LANG/MSG/LANG_MSG_FRA.py
+++ LANG/MSG/LANG_MSG_FRA.py
@@ -1,12 +1,12 @@
 STRINGS_MSG = {'MSG_TEST': "Il est {salle}",
                'MSG_THE_TIME_IS': ["Il est {heure} heures et {minute} minutes.",
                                    "L'heure actuelle est {heure}:{minute}.",
-                                   "Le temps est {heure}h{minute}.",
                                    "Il est {heure}:{minute}.",
-                                   "La montre indique {heure}:{minute}."],
+                                   "La montre indique {heure}:{minute}.",
+                                   "Mon horloge interne indique {heure}:{minute}."],
                'MSG_INDICATE_ROOM_FOR_CLASS': ["Vous avez cours dans la salle {0}}.",
                                                "Votre cours a lieu dans la salle {0}}.",
                                                "Le cours se déroule dans la salle {0}}.",
                                                "Vous trouverez votre cours dans la salle {0}}.",
-                                               "Le lieu de votre cours est la salle {0}."],
+                                               "Le lieu de votre cours est la salle {0}."]
                }
