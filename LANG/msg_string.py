8a1b0ef6cb3162f7306469af8860f34de89b542c
Sierra-Springs <nathanael-1999@hotmail.fr>
2023-01-05 14:44:25 +0100

    begin to implement msg_string_autogen

--- LANG/msg_string.py
+++ LANG/msg_string.py
@@ -1,9 +1,23 @@
 from msg_string_base import *
+import re
+import os
+import sys
+from pathlib import Path
+
+
+source_root = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+if source_root not in sys.path:
+    sys.path.append(source_root)
 
 
 keys_handled = []
-with open("LANG/msg_string_autogen_func.py", "w") as msg_string_autogen_func:
+with open(source_root / "LANG" / "msg_string_autogen_func.py", "w") as msg_string_autogen_func:
     msg_string_autogen_func.write("from msg_string_base import *\n\n")
+
+    accolades_regex = re.compile(r'{(?:.*)}')
+    mo = accolades_regex.search('hello {pomme} patate {frite}')
+    print(mo.groups())
+
     for key in STRINGS_MSG:
         msg_string_autogen_func.write(f"def {key}():")
         msg_string_autogen_func.write("\treturn construct_string(locals())\n\n")
@@ -32,4 +46,8 @@ def MSG_INDICATE_ROOM_FOR_CLASS(salle):
 if __name__ == "__main__":
     print(MSG_THE_TIME_IS(heure="12", minute="30"))
     print(MSG_TEST("C104"))
-    print(MSG_THE_WEATHER_IS("Clear", 4.52))
+    print(MSG_THE_WEATHER_IS("Clear", 4, 52))
+
+    accolades_regex = re.compile(r'{(?:.*)}')
+    mo = accolades_regex.search('hello {pomme} patate {frite}')
+    print(mo.groups())
---
+++ LANG/msg_string_autogen_func.py
@@ -0,0 +1,10 @@
+from msg_string_base import *
+
+def MSG_TEST():	return construct_string(locals())
+
+def MSG_THE_TIME_IS():	return construct_string(locals())
+
+def MSG_INDICATE_ROOM_FOR_CLASS():	return construct_string(locals())
+
+def MSG_THE_WEATHER_IS():	return construct_string(locals())
+
